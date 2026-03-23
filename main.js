cce9c7b5abebbd5df4912a4d14e4b5631cbb034b
Viviane Aparecida Martins Correa <correa.viviane@escola.pr.gov.br>
2026-03-23 08:23:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function atualizaCronometro(){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];  
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
 
